60b1ebcc5c413395dcd23528f840efbf325ae96c
Albert <albertirura11@gmail.com>
2024-11-11 19:08:01 +0300

    resend

--- api/submit.js
+++ api/submit.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const { name, country, city, email, product, quantity, price, message } = req.body;
     
     await resend.emails.send({
-      from: 'extrafreshfarm@resend.dev',
+      from: email,
       to: 'albertirura11@gmail.com',
       subject: `New Enquiry Form Submission from ${name}`,
       text: `
