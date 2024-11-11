558db61c496cf361d493817cbc0dcc617f86d8e0
Albert <albertirura11@gmail.com>
2024-11-11 19:40:40 +0300

    configure env

--- api/submit.js
+++ api/submit.js
@@ -1,6 +1,7 @@
 import { Resend } from 'resend';
+import dotenv from 'dotenv';
 
-require('dotenv').config()
+dotenv.config();
 const resend = new Resend(process.env.RESEND_KEY);
 
 export default async function handler(req, res) {
