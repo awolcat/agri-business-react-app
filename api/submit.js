5a22e163a183f6d4117e79dbf1d9a5726980ddbc
Albert <albertirura11@gmail.com>
2024-11-13 17:46:17 +0300

    Submit form fix

--- api/submit.js
+++ api/submit.js
@@ -8,20 +8,6 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: `Method not allowed. Received a ${req.method} instead of POST request` });
   }
 
-  const createContact = async (email, name) => {
-    
-    const response = await resend.contacts.create({
-      email: email,
-      firstName: name,
-      unsubscribed: false,
-      audienceId: '2cd3c10b-72d4-4c57-96e7-9620e367f5c5',
-    });
-    console.log("Create Contact ", response);
-    if (!response.ok) {
-      throw new Error(response || 'Error creating contact');
-    }
-  }
-
   try {
     const { name, country, city, email, product, quantity, price, message } = req.body;
     await resend.emails.send({
@@ -40,7 +26,12 @@ export default async function handler(req, res) {
         Message: ${message}
       `,
     });
-    await createContact(email, name);
+    await resend.contacts.create({
+      email: email,
+      firstName: name,
+      unsubscribed: false,
+      audienceId: '2cd3c10b-72d4-4c57-96e7-9620e367f5c5',
+    });
     return res.status(200).json({ message: 'Email sent successfully' });
 
   } catch (error) {
