9fbb59dc6ee2c3200a4806a24f03a1eabc00137d
Albert <albertirura11@gmail.com>
2024-11-13 15:59:49 +0300

    create contact on enquiry

--- api/submit.js
+++ api/submit.js
@@ -2,28 +2,32 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_KEY);
 
-const createContact = async (email, name) => {
-    
-  const response = await resend.contacts.create({
-    email: email,
-    firstName: name,
-    unsubscribed: false,
-    audienceId: '2cd3c10b-72d4-4c57-96e7-9620e367f5c5',
-  });
-  if (!response.ok) {
-    const error = await response.json();
-    throw new Error(error.message || 'Error creating contact');
-  }
-}
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: `Method not allowed. Received a ${req.method} instead of POST request` });
   }
 
+  const createContact = async (email, name) => {
+    
+    const response = await resend.contacts.create({
+      email: email,
+      firstName: name,
+      unsubscribed: false,
+      audienceId: '2cd3c10b-72d4-4c57-96e7-9620e367f5c5',
+    });
+    const res = await response.json();
+    console.log(res);
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message || 'Error creating contact');
+    }
+  }
+
   try {
     const { name, country, city, email, product, quantity, price, message } = req.body;
     //emilywairimu97@icloud.com
+    message = message || 'No message provided';
     await resend.emails.send({
       from: 'Test <emily@extrafreshfarm.com>',
       to: ['albertirura11@gmail.com'],
