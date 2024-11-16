8658f9ee095c9e8c9972031df1a3e46f1beaa067
Albert <albertirura11@gmail.com>
2024-11-16 10:32:41 +0300

    footer content

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -144,7 +144,7 @@ const LandingPage = () => {
       {/* Footer */}
       <footer className="bg-green-800 text-white py-8">
         <div className="container mx-auto px-4 text-center">
-          <p>&copy; 2024 Agricultural Business. All rights reserved.</p>
+          <p>&copy; 2024 Extra Fresh Farm.</p>
         </div>
       </footer>
     </div>
