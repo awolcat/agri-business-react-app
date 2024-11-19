7d1a4d4497ccde5f0a0e8502972be7dc0883d38b
Albert <albertirura11@gmail.com>
2024-11-19 17:46:37 +0300

    updated success modal items spacing

--- src/components/SuccessSubmitModal.jsx
+++ src/components/SuccessSubmitModal.jsx
@@ -15,8 +15,8 @@ const SuccessModal = ({ isOpen, onClose, message = "Message sent successfully!" 
 
     return (
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-80 top-50">
-        <div className="bg-green-50 flex flex-col gap-20 border border-green-200 p-6 rounded-lg shadow-xl max-w-sm w-full">
-          <div className="flex justify-between items-center mb-4">
+        <div className="bg-green-50 flex flex-col gap-5 border border-green-200 p-6 rounded-lg shadow-xl max-w-sm w-full">
+          <div className="flex items-center justify-center mb-4">
             <h2 className="text-green-700 font-semibold">Success</h2>
           </div>
           <p className="text-green-800">{message}</p>
