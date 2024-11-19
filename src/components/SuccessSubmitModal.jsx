e37cb42426566f93a075bce4359dd10dd0d3728a
Albert <albertirura11@gmail.com>
2024-11-19 16:19:07 +0300

    updated form submit success modal

--- src/components/SuccessSubmitModal.jsx
+++ src/components/SuccessSubmitModal.jsx
@@ -1,7 +1,7 @@
 const SuccessModal = ({ onClose, message = "Message sent successfully!" }) => {
   
     return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-white opacity-80 fixed">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-white fixed top-0 left-0 w-full h-full">
         <div className="bg-green-50 border border-green-200 p-6 rounded-lg shadow-xl max-w-sm w-full">
           <div className="flex justify-between items-center mb-4">
             <h2 className="text-green-700 font-semibold">Success</h2>
